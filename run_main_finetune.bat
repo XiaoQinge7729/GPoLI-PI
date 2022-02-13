2912e2fab7d587670b0b4d153fd65e0ec4c8c5c0
XiaoQinge7729 <pretty_xixi@qq.com>
2022-02-13 08:17:47 +0800

    add pretrained demo

--- run_main_finetune.bat
+++ run_main_finetune.bat
@@ -1,5 +1,5 @@
 @echo off
 
-python main.py --moo-mode --use-gail 7 --use-critic 2 --trained-model "trained_models/20220212_122358/EP_2005_-6_0.pt" --gail-model  "trained_models/20220212_122358/EP_2005_-6_0_d.pt"
+python main.py --moo-mode --use-gail 7 --use-critic 2 --trained-model "trained_models/20220213_012544/EP_4000_-12_0.pt" --gail-model  "trained_models/20220213_012544/EP_4000_-12_0_d.pt"
 
 pause
